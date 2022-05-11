25dea169a26de52f6b87ddf8c0ad389b279a2f33
Lasse <lassewolpmann@outlook.de>
2022-05-11 18:31:46 +0300

    code clean up

--- src/App.svelte
+++ src/App.svelte
@@ -1,108 +1,74 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let current = 'Race'
+	let current = 'Race';
 
-	const eventNames = [
-		"FORMULA 1 GULF AIR BAHRAIN GRAND PRIX 2022",
-		"FORMULA 1 STC SAUDI ARABIAN GRAND PRIX 2022",
-		"FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022",
-		"FORMULA 1 ROLEX GRAN PREMIO DEL MADE IN ITALY E DELL'EMILIA-ROMAGNA 2022",
-		"FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2022",
-		"FORMULA 1 PIRELLI GRAN PREMIO DE ESPAÑA 2022",
-		"FORMULA 1 GRAND PRIX DE MONACO 2022",
-		"FORMULA 1 AZERBAIJAN GRAND PRIX 2022",
-		"FORMULA 1 GRAND PRIX DU CANADA 2022",
-		"FORMULA 1 LENOVO BRITISH GRAND PRIX 2022",
-		"FORMULA 1 GROSSER PREIS VON ÖSTERREICH 2022",
-		"FORMULA 1 LENOVO GRAND PRIX DE FRANCE 2022",
-		"FORMULA 1 ARAMCO MAGYAR NAGYDÍJ 2022",
-		"FORMULA 1 ROLEX BELGIAN GRAND PRIX 2022",
-		"FORMULA 1 HEINEKEN DUTCH GRAND PRIX 2022",
-		"FORMULA 1 PIRELLI GRAN PREMIO D'ITALIA 2022",
-		"FORMULA 1 SINGAPORE GRAND PRIX 2022",
-		"FORMULA 1 JAPANESE GRAND PRIX 2022",
-		"FORMULA 1 ARAMCO UNITED STATES GRAND PRIX 2022",
-		"FORMULA 1 GRAN PREMIO DE LA CIUDAD DE MÉXICO 2022",
-		"FORMULA 1 HEINEKEN GRANDE PRÊMIO DE SÃO PAULO 2022",
-		"FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2022"
-	]
+	let season = [];
 
-	let season = []
-
-	let timezoneOffset = undefined
+	let timezoneOffset;
 
 	let nextEvent = undefined
 	let nextEventName = undefined
 	let nextEventTrack = undefined
 	let nextEventSessions = []
 	
-	const mountTimestamp = new Date().getTime()
-	let nextRaceTimestamp = undefined
-	let deltaToRaceSeconds = undefined
+	const mountTimestamp = new Date().getTime();
 
-	let daysUntilNextRace = undefined
-	let hoursUntilNextRace = undefined
-	let minutesUntilNextRace = undefined
-	let secondsUntilNextRace = undefined
+	let nextRaceTimestamp, deltaToRaceSeconds;
+
+	let daysUntilNextRace, hoursUntilNextRace, minutesUntilNextRace, secondsUntilNextRace;
+
 
 	onMount(() => {
 		fetch('https://ergast.com/api/f1/' + new Date().getFullYear() + '.json')
 			.then(response => response.json())
-			.then(result => season = result.MRData.RaceTable.Races)
-			.then(season => {
-				for (let i = 0; i < season.length; i++) {
-					if (new Date(season[i].date).getTime() > mountTimestamp) {
-						timezoneOffset = (new Date().getTimezoneOffset() * -1) * 60
+			.then(result => {
+				season = result['MRData']['RaceTable']['Races'];
+				season = season.filter((item) => new Date(item.date).getTime() > mountTimestamp);
 
-						nextEvent = season[i]
-						nextEventTrack = nextEvent.Circuit.circuitName
-						nextEventSessions = Object.keys(nextEvent).slice(-4)
-						nextEventName = eventNames[i]
+				if (season.length > 0) {
+					timezoneOffset = (new Date().getTimezoneOffset() * -1) * 60;
 
-						nextRaceTimestamp = Date.parse(nextEvent.date + ' ' + nextEvent.time.slice(0, -1))
+					nextEvent = season[0];
+					nextEventTrack = nextEvent['Circuit']['circuitName'];
+					nextEventSessions = Object.keys(nextEvent).slice(-4);
+					nextEventName = nextEvent['raceName'];
 
-						deltaToRaceSeconds = ((nextRaceTimestamp - mountTimestamp) / 1000) + timezoneOffset
+					nextRaceTimestamp = Date.parse(nextEvent.date + ' ' + nextEvent.time.slice(0, -1));
 
-						daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400)
-						hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600)
-						minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60)
-						secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60)
+					deltaToRaceSeconds = ((nextRaceTimestamp - mountTimestamp) / 1000) + timezoneOffset;
 
-						break
-					}
+					daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400);
+					hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600);
+					minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60);
+					secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60);
+
+					setInterval(() => {
+						deltaToRaceSeconds -= 1
+
+						daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400);
+						hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600);
+						minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60);
+						secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60);
+					}, 1000);
+
+				} else {
+					console.log('no races left');
 				}
-
-				setInterval(() => {
-					deltaToRaceSeconds -= 1
-					
-					daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400)
-					hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600)
-					minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60)
-					secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60)
-				}, 1000)
-			})
+			});
 	});
 
 	function calculateDeltaToSession(session) {
-		let nextSessionDate = undefined
-		let nextSessionTime = undefined
+		let nextSessionDate = session === 'Race' ? nextEvent.date : nextEvent[session].date;
+		let nextSessionTime = session === 'Race' ? nextEvent.time : nextEvent[session].time;
 
-		if (session === 'Race') {
-			nextSessionDate = nextEvent.date
-			nextSessionTime = nextEvent.time
-		} else {
-			nextSessionDate = nextEvent[session].date
-			nextSessionTime = nextEvent[session].time
-		}
+		nextRaceTimestamp = Date.parse(nextSessionDate + ' ' + nextSessionTime.slice(0, -1));
+		deltaToRaceSeconds = ((nextRaceTimestamp - new Date().getTime()) / 1000) + timezoneOffset;
 
-		nextRaceTimestamp = Date.parse(nextSessionDate + ' ' + nextSessionTime.slice(0, -1))
-		deltaToRaceSeconds = ((nextRaceTimestamp - new Date().getTime()) / 1000) + timezoneOffset
-
-		daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400)
-		hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600)
-		minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60)
-		secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60)
+		daysUntilNextRace = Math.floor(deltaToRaceSeconds / 86400);
+		hoursUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 / 3600);
+		minutesUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 / 60);
+		secondsUntilNextRace = Math.floor(deltaToRaceSeconds % 86400 % 3600 % 60);
 	}
 </script>
 
@@ -127,7 +93,7 @@
 		cursor: pointer;
 		font-weight: bold;
 		font-size: 1.25rem;
-		margin: 20px 20px 0px 20px;
+		margin: 20px 20px 0 20px;
 		border: none;
 	}
 
@@ -148,7 +114,7 @@
 		min-width: 200px;
 		max-width: 200px;
 		min-height: 200px;
-		max-width: 200px;
+		max-height: 200px;
 		margin: 20px;
 	}
 
